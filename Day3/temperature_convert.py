7ce991e303d9d08e0a1999e241046880c83fae94
adarsh-k-10 <adarshkraghu@gmail.com>
2026-02-25 17:16:40 +0530

    add temperature_convert

---
+++ Day3/temperature_convert.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        kelvin = round(celsius + 273.15, 5)
+        fahrenheit = round(celsius * 1.80 + 32.00, 5)
+        
+        return [kelvin, fahrenheit]
