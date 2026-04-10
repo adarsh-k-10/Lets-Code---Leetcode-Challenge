5ad47795a7db3008ada48876550fe4eb9eb0680d
adarsh-k-10 <adarshkraghu@gmail.com>
2026-04-10 09:49:24 +0530

    add n_unique_number.py

---
+++ Day47/n_unique_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sumZero(self, n):
+        
+        res = []
+
+        for i in range(1,n//2+1):
+            res.append(i)
+            res.append(i*-1)
+        
+        if n%2 == 1:
+            res.append(0)
+            
+        return res
